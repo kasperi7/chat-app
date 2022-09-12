7b0468e22c474b2abeec96c141729cde7a0885d0
Kasperi <kasperih@metropolia.fi>
2022-09-12 14:19:20 +0300

    msg indentification

--- index.js
+++ index.js
@@ -1,34 +1,36 @@
-'use strict';
+"use strict";
 
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 const users = [];
 
-io.on('connection', (socket) => {
-  console.log('a user connected', socket.id);
-  
-  socket.on('join', (username) => {
-    users.push({username: username, id: socket.id});
-    console.log('users connected:', users);
-    socket.emit('response', 'Joined with username ' + username);
+io.on("connection", (socket) => {
+  console.log("a user connected", socket.id);
+
+  socket.on("join", (username) => {
+    users.push({ username: username, id: socket.id });
+    console.log("users connected:", users);
+    socket.emit("response", "Joined with username " + username);
   });
 
-  socket.on('disconnect', () => {
-    console.log('a user disconnected', socket.id);
+  socket.on("disconnect", () => {
+    console.log("a user disconnected", socket.id);
     // TODO: remove user with socket.id form users array
   });
 
-  socket.on('chat message', (msg) => {
-    console.log('message: ', msg);
-    io.emit('chat message', msg);
+  socket.on("chat message", (msg) => {
+    let userObject = users.find((user) => user.id === socket.id);
+    let username = userObject.username;
+    console.log("message: ", username, msg);
+    io.emit("chat message", msg, username);
   });
 });
 
 http.listen(3000, () => {
-  console.log('listening on port 3000');
+  console.log("listening on port 3000");
 });
--- public/chat.js
+++ public/chat.js
@@ -1,28 +1,28 @@
-'use strict';
+"use strict";
 
-//const socket = io('http://localhost:3000');
-const socket = io('https://servu.norwayeast.cloudapp.azure.com');
+const socket = io("http://localhost:3000");
+//const socket = io('https://servu.norwayeast.cloudapp.azure.com');
 
-document.querySelector('#msg-input').addEventListener('submit', (event) => {
+document.querySelector("#msg-input").addEventListener("submit", (event) => {
   event.preventDefault();
-  const inp = document.getElementById('m');
-  socket.emit('chat message', inp.value);
-  inp.value = '';
+  const inp = document.getElementById("m");
+  socket.emit("chat message", inp.value);
+  inp.value = "";
 });
 
-document.querySelector('#join').addEventListener('submit', (event) => {
+document.querySelector("#join").addEventListener("submit", (event) => {
   event.preventDefault();
-  const inp = document.getElementById('username');
-  socket.emit('join', inp.value);
-  inp.value = '';
+  const username = document.getElementById("username");
+  socket.emit("join", username.value);
+  username.value = "";
 });
 
-socket.on('chat message', (msg) => {
-  const item = document.createElement('li');
-  item.innerHTML = msg;
-  document.getElementById('messages').appendChild(item);
+socket.on("chat message", (msg, username) => {
+  const item = document.createElement("li");
+  item.innerHTML = username + ": " + msg;
+  document.getElementById("messages").appendChild(item);
 });
 
-socket.on('response', (msg) => {
+socket.on("response", (msg) => {
   console.log(msg);
 });
